7d5432c9223188cf75dbbccaff6a31b36cdea459
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-03-09 14:02:30 +0100

    Got the 5 letter lingo working

--- Program.js
+++ Program.js
@@ -6,86 +6,86 @@ var gAttempts = 1;
 var gSubmits = 5;
 
 function startGame() {
-    var userInput = document.getElementById('UserInput').value;
-    document.getElementById("Attempts").innerHTML = gSubmits + " attempts left.";
-    document.getElementById("1_0").innerHTML = gLetters[0];
-    var inputLetters = userInput.split("");
+  var userInput = document.getElementById('UserInput').value;
+  document.getElementById("Attempts").innerHTML = gSubmits + " attempts left.";
+  document.getElementById("1_0").innerHTML = gLetters[0];
+  var inputLetters = userInput.split("");
 }
 
 function checkInput() {
-    var cols = document.getElementById("radio").length;
-    console.log(cols);
-    // copy input to empty row
-    var checkInput = document.getElementById("UserInput").value;
-    var inputArray = checkInput.split("");
-    for (i = 0; i < 5; i++) {
-        var letterNum = gAttempts.toString() + "_" + i;
+  //var cols = document.getElementById("radio").length;
+  //console.log(cols);
+  // copy input to empty row
+  var checkInput = document.getElementById("UserInput").value;
+  var inputArray = checkInput.split("");
+  for (i = 0; i < 5; i++) {
+    var letterNum = gAttempts.toString() + "_" + i;
 
-        //good
-        if (inputArray[i] == gLetters[i]) {
-            document.getElementById(letterNum).style.backgroundColor = "red";
-            //Contains
-        } else if (inputArray.indexOf(gLetters[i]) > -1) {
-            document.getElementById(letterNum).style.backgroundColor = "yellow";
-            //Wrong
-        } else {
-
-        }
+    //good
+    if (inputArray[i] == gLetters[i]) {
+      document.getElementById(letterNum).style.backgroundColor = "green";
+      //Contains
+    } else if (gLetters.indexOf(inputArray[i]) > -1) {
+      document.getElementById(letterNum).style.backgroundColor = "yellow";
     }
-    if (checkInput.length == 5) {
-        CopyInputToEmptyRow();
-        gSubmits--;
-        document.getElementById("Attempts").innerHTML = gSubmits + " attempts left.";
-        if (document.getElementById("UserInput").value == gAnswer) {
-            document.getElementById("Game").innerHTML = "<h1 class='YouWon'>You won! <br/>" + gAnswer + " was correct!</h1>";
-            document.getElementById("msgBox").innerHTML = "";
-            document.getElementById("smallMsg").innerHTML = "";
-            $("button").toggleClass("invisible");
-        }
+  }
+  if (checkInput.length == 5) {
+    CopyInputToEmptyRow();
+    gSubmits--;
+    document.getElementById("Attempts").innerHTML = gSubmits + " attempts left.";
+    if (checkInput == gAnswer) {
+      document.getElementById("Game").innerHTML = "<h1 class='YouWon'>You won! <br/>" + gAnswer + " was correct!</h1>";
+      document.getElementById("msgBox").innerHTML = "";
+      document.getElementById("smallMsg").innerHTML = "";
+      $("button").toggleClass("invisible");
     }
-    if (gSubmits < 1) {
-        document.getElementById("Game").innerHTML = "<h1 class='GameOver'> BIEEEMMMM you're ded!</h1><br/><div class='Gun' style='width: 120%;height: 200px;margin: auto;float: left;margin-top: -380px;'><img src='https://pbs.twimg.com/profile_images/1517890377/Drool_face_400x400.png' style='width: auto; height: 100px; float: left;border-radius: 50%;'/><img src='https://upload.wikimedia.org/wikipedia/commons/d/d0/Kriss_Vector_SMG_Realistic.png' style='width: auto; height: 100px; float: right;'/></div>";
-        document.getElementById("msgBox").innerHTML = "";
-        document.getElementById("smallMsg").innerHTML = "";
-        $("button").toggleClass("invisible");
-    }
-    if (checkInput.length !== 5) {
-        alert("Please enter at 5 characters!");
-    }
+    console.log("Checking checkInput: " + checkInput);
+    console.log("Checking gAnswer: " + gAnswer);
+  }
+  if (gSubmits < 1) {
+    document.getElementById("Game").innerHTML = "<h1 class='GameOver'> BIEEEMMMM you're ded!</h1><br/><div class='Gun' style='width: 120%;height: 200px;margin: auto;float: left;margin-top: -380px;'><img src='https://pbs.twimg.com/profile_images/1517890377/Drool_face_400x400.png' style='width: auto; height: 100px; float: left;border-radius: 50%;'/><img src='https://upload.wikimedia.org/wikipedia/commons/d/d0/Kriss_Vector_SMG_Realistic.png' style='width: auto; height: 100px; float: right;'/></div>";
+    document.getElementById("msgBox").innerHTML = "";
+    document.getElementById("smallMsg").innerHTML = "";
+    $("button").toggleClass("invisible");
+  }
+  if (checkInput.length !== 5) {
+    alert("Please enter at 5 characters!");
+  }
 
 }
 
 function CopyInputToEmptyRow() {
-    var userInput = document.getElementById('UserInput').value;
-    var inputArray = userInput.split("");
-    var curRow = document.getElementById("row" + gAttempts);
-    var clicks = document.getElementsByTagName('input' [0]);
+  var userInput = document.getElementById('UserInput').value;
+  var inputArray = userInput.split("");
+  var curRow = document.getElementById("row" + gAttempts);
+  var clicks = document.getElementsByTagName('input' [0]);
+  if (gAttempts <= 4) {
+    clicks.onclick = gAttempts++;
+    var curRow = document.getElementById("row" + (gRows + 1));
+    var displayLetter1 = document.getElementById((gRows + 1) + "_0").innerHTML = inputArray[0];
+    var displayLetter2 = document.getElementById((gRows + 1) + "_1").innerHTML = inputArray[1];
+    var displayLetter3 = document.getElementById((gRows + 1) + "_2").innerHTML = inputArray[2];
+    var displayLetter4 = document.getElementById((gRows + 1) + "_3").innerHTML = inputArray[3];
+    var displayLetter5 = document.getElementById((gRows + 1) + "_4").innerHTML = inputArray[4];
+  }
 
-    clicks.onclick = gRows++;
-    if (gAttempts <= 4) {
-        //ROW 1
-        clicks.onclick = gAttempts++;
-        var curRow = document.getElementById("row" + gRows);
-        var displayLetter1 = document.getElementById(gRows + "_0").innerHTML = inputArray[0];
-        var displayLetter2 = document.getElementById(gRows + "_1").innerHTML = inputArray[1];
-        var displayLetter3 = document.getElementById(gRows + "_2").innerHTML = inputArray[2];
-        var displayLetter4 = document.getElementById(gRows + "_3").innerHTML = inputArray[3];
-        var displayLetter5 = document.getElementById(gRows + "_4").innerHTML = inputArray[4];
-    }
+  clicks.onclick = gRows++;
 
-    document.getElementById("UserInput").value = '';
+  document.getElementById("UserInput").value = '';
 }
 
 //Reloads game
 function restartGame() {
-    location.reload();
+  location.reload();
 }
 
 //Detects "Enter" keypress
-document.onkeydown = function () {
-    if (window.event.keyCode == '13') {
-        checkInput();
-    }
+document.onkeydown = function() {
+  if (window.event.keyCode == '13') {
+    checkInput();
+  }
 }
 
 startGame();
+
+console.log(gLetters);
--- generateGrid.js
+++ generateGrid.js
@@ -1,10 +1,11 @@
+/*
 function generateGrid() {
   var addCols = 4;
   var colsels = document.getElementById("radio" + addCols);
   var cols = 5;
   var radios = 3;
   var table = document.getElementById('Circle');
-  var clearTable = document.getElementById("Circle").innerHTML = "";
+  //var clearTable = document.getElementById("Circle").innerHTML = "";
 
   document.getElementById('radio0').onclick = function click() {
     cols = 5;
@@ -46,28 +47,5 @@ function generateGrid() {
 
     //}
   }
-
-  // document.getElementById("radio0").onclick = (cols = 5);
-  // document.getElementById("radio1").onclick = (cols = 6);
-  // document.getElementById("radio2").onclick = (cols = 7);
-  // document.getElementById("radio3").onclick = (cols = 8);
 }
-
-// function generateGrid(v) {
-//   var i = 0;
-//   var e = document.getElementById("Game"); // whatever you want to append the rows to:
-//   for (i = 0; i < v; i++) {
-//     var row = document.createElement("div");
-//     row.className = "row_" + i;
-//     for (var x = 1; x <= v; x++) {
-//       var cell = document.createElement("div");
-//       cell.className = "square default";
-//       cell.innerText = (i * v) + x;
-//       row.appendChild(cell);
-//     }
-//     e.appendChild(row);
-//   }
-//   document.getElementById("row" + i).innerHTML = e.innerHTML;
-//   console.log(e.innerHTML);
-//
-// }
+*/
